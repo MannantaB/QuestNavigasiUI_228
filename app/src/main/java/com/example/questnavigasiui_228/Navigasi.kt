7d9005cb85f3acf65f11441b143e506c93c753a9
MannantaB <mannanta18@gmail.com>
2025-11-06 09:19:58 +0700

    add fungsi DataApp dengan NavController untuk navigasi antar halaman

--- app/src/main/java/com/example/questnavigasiui_228/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_228/Navigasi.kt
@@ -1,6 +1,20 @@
 package com.example.questnavigasiui_228
 
+import android.widget.MediaController
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulir,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+    ){
+
 }
