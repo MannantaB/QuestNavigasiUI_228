680207681e09f920a2cecca56309a6c1fcf217d6
MannantaB <mannanta18@gmail.com>
2025-11-06 09:25:21 +0700

    add NavHost dengan startDestination Formulir

--- app/src/main/java/com/example/questnavigasiui_228/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_228/Navigasi.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -18,6 +19,9 @@ fun DataApp(
     modifier: Modifier
     ){
     Scaffold { isiRuang->
-
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name
+        )
     }
 }
